30a7fa18a3e9d08bc6fbd90a768a0f7842d854b9
Riryan <anthonyfmitchell@gmail.com>
2026-09-25 18:01:23 -0500

    Push

--- MMODashboard/DashboardConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace MMODashboard;
-
-internal sealed class DashboardConfig
-{
-    public string BuildGatewayBat { get; set; } = "";
-    public string BuildGameServerBat { get; set; } = "";
-    public string RunGatewayBat { get; set; } = "";
-    public string RunGameServerBat { get; set; } = "";
-}
